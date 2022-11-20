469a28e85c807d46f5657937344471f5a2a59644
Julya <Afanasevajv@gmail.com>
2022-11-20 15:55:37 +0300

    If-else

---
+++ if-else/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользовател: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+
+{
+   Console.WriteLine("Ура, это же МАША!");
+}
+
+else
+
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
